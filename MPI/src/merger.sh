7355478fcf889d324097b8789ccabba51e0cc880
pgm-n117 <pgm.atctone@gmail.com>
2020-10-27 11:51:06 +0000

    añadidos scripts para la creación y fusión de trazas de ejecución para Extrae y Paraver

--- MPI/src/merger.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-mpirun.openmpi -np 4 -hostfile ./hostfile -mca btl_base_warn_component_unused=0 --oversubscribe -allow-run-as-root mpi2prv -dump-without-time -v -f TRACE.mpits -e matmul_AdvMpi.o -o TRAZA_MPI.prv
-

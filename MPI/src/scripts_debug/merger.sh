28a78906b05061ba614ec4fc2817358880f9d407
pablo <pgm.atctone@gmail.com>
2023-02-23 08:37:06 +0100

    README updated

--- MPI/src/scripts_debug/merger.sh
+++ MPI/src/scripts_debug/merger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-mpirun.openmpi -np 16 -hostfile /home/pablo/mpi-sdn/MPI/src/hostfile -mca btl_base_warn_component_unused=0 --oversubscribe -allow-run-as-root mpimpi2prv -dump-without-time -v -f  TRACE.mpits -e /home/pablo/mpi-sdn/MPI/src/matmul_SimpleMpi_DEBUG.o
+mpirun.openmpi -np 16 -hostfile ~/mpi-sdn/MPI/src/hostfile -mca btl_base_warn_component_unused=0 --oversubscribe -allow-run-as-root mpimpi2prv -dump-without-time -v -f  TRACE.mpits -e ~/mpi-sdn/MPI/src/matmul_SimpleMpi_DEBUG.o
 
